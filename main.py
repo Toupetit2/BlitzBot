095fa1b9a89ede5a90490dfe5677f884ba486819
Masktendo <148429615+Masktendo@users.noreply.github.com>
2024-04-15 17:41:47 +0200

    Main (modified)
    Map + Murs

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 import sys
 
-from player import *
+from player import*
 
-LARGUEUR, HAUTEUR = 900, 650
+
+LARGEUR, HAUTEUR = 900, 650
 pygame.init()
 
-screen = pygame.display.set_mode((LARGUEUR, HAUTEUR))
+screen = pygame.display.set_mode((LARGEUR, HAUTEUR))
 clock = pygame.time.Clock()
 FPS = 60
 
@@ -24,6 +25,25 @@ player = Player()
 
 
 # MAP
+def dessine_map() : # Dessine la map sous forme de grille 
+    case = 65
+    for x in range(0, LARGEUR,case) :
+        for y in range(0, HAUTEUR, case) :
+            rect =pygame.Rect(x,y, case,case)
+            pygame.draw.rect(screen, (255,255,255), rect, 1)
+
+#Mur
+def dessine_mur() :
+    # Les coordonnées sont comptées de 65 en 65 (pour l'instant)
+    #LES LIGNES CI DESSOUS SERVENT DE TEST (les coodonnées équivalante sont indiquées à côté)
+    
+    pygame.draw.rect(screen,(0,0,0),(65, 0,65,65 )) # = (1,0)
+    pygame.draw.rect(screen,(0,0,0),(65, 65,65,65 )) # = (1,1)
+    pygame.draw.rect(screen,(0,0,0),(130,65,65,65))# =(1,2)     
+    pygame.draw.rect(screen,(0,0,0),(260,130,65,65 ))  # =(4,2)
+    pygame.draw.rect(screen,(0,0,0),(130, 260,65,65 )) # = (2,4)
+    pygame.draw.rect(screen,(0,0,0),(195, 260,65,65 )) # = (3,4)
+    pygame.draw.rect(screen,(0,0,0),(390, 260,65,65 )) # = (6,4)
 
 # TEMPORAIREMENT 65 DE LARGE POUR UNE TUILE
 
@@ -43,8 +63,8 @@ while True:
     player.move_cam()
     
     # AFFICHAGE TEMPORAIRE background map
-    for i in range(10):
-        for j in range(10):
+    for i in range(15):
+        for j in range(15):
             pygame.draw.rect(screen, (i * 5 + 30, j * 5 + 30, 100),(i*65, j*65, 65, 65))
     
     #Afficher joueur
@@ -52,7 +72,8 @@ while True:
     #Afficher direction
     pygame.draw.line(screen, (255, 255, 0), (player.x * 65, player.y * 65), position_devant_joueur(player, 1))
 
-
+    dessine_map()
+    dessine_mur()
     
     #print(round(player.x, 1), round(player.y, 1), player.orientation)
     pygame.display.update()
